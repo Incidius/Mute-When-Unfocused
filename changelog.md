c90b6ebfc44f7cb176322f8166f78e791d906129
Incidius <89553616+Incidius@users.noreply.github.com>
2026-04-06 10:31:53 +0100

    V1.0.1
    Version 1.0.1 - Bugfix when exiting GD unfocused. Adding tags.

--- changelog.md
+++ changelog.md
@@ -1,4 +1,5 @@
-# 1.0.0
+# 1.0.1
 
 * Version 1.0.0 - initial release
+* Version 1.0.1 - Bugfix when exiting GD unfocused. Adding tags.
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <Geode/Geode.hpp>
 
 #include <Geode/modify/CCEGLView.hpp>
+#include <Geode/modify/AppDelegate.hpp>
 
 
 using namespace geode::prelude;
 
+namespace {
+    float music = 0;
+    float sfx = 0;
+    bool savedVolume = false;
+}
 
 #ifdef GEODE_IS_WINDOWS
 class $modify(CCEGLView) {
@@ -13,8 +19,7 @@ class $modify(CCEGLView) {
         CCEGLView::pollEvents();
 
 
-        static float music = 0;
-        static float sfx = 0;
+        
         static bool focused = false;
         FMODAudioEngine* sound = FMODAudioEngine::sharedEngine();
 
@@ -36,6 +41,7 @@ class $modify(CCEGLView) {
              
             music = sound->m_musicVolume;
             sfx = sound->m_sfxVolume;
+            savedVolume = true;
             sound->setBackgroundMusicVolume(0);
             sound->setEffectsVolume(0);
             
@@ -43,5 +49,24 @@ class $modify(CCEGLView) {
         }
         
     }
+
+
 };
+
+class $modify(AppDelegate) {
+    void platformShutdown() {
+        AppDelegate::platformShutdown();
+
+        FMODAudioEngine* sound = FMODAudioEngine::sharedEngine();
+
+        if (savedVolume) {
+            sound->setBackgroundMusicVolume(music);
+            sound->setEffectsVolume(sfx);
+            //fixes a bug where the volume would be muted on reopen and focus, if gd is closed while unfocused
+        }
+    }
+};
+
 #endif
+
+
